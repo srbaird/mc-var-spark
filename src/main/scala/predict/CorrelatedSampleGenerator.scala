bbf46f7a458e79e8de32c5f62a8f22e85308b84f
sbaird02 <sbaird02@mail.bbk.ac.uk>
2016-07-13 14:01:54 +0100

    Trait CorrelatedSampleGenerator

---
+++ src/main/scala/predict/CorrelatedSampleGenerator.scala
@@ -0,0 +1,19 @@
+package main.scala.predict
+
+/**
+ * Generate random samples of correlated factor variates
+ */
+trait CorrelatedSampleGenerator {
+
+  val defaultNumberOfVariates = 1L
+  /**
+   * Generate a default number of variates correlated to the supplied matrix
+   */
+  def sampleCorrelated(f: Array[Array[Double]]): Array[Array[Double]] = sampleCorrelated(defaultNumberOfVariates, f)
+
+  /**
+   * Generate a n number of variates correlated to the supplied matrix
+   */
+  def sampleCorrelated(n: Long, f: Array[Array[Double]]): Array[Array[Double]]
+
+}
