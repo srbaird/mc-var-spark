a486e56929b22fa91d5f44365d93a2fb0377f9a9
sbaird02 <sbaird02@mail.bbk.ac.uk>
2016-08-02 07:26:59 +0100

    Skeleton implementation of CovarianceValuePredictor test

--- src/test/scala/predict/CovarianceValuePredictorWithDITest.scala
+++ src/test/scala/predict/CovarianceValuePredictorWithDITest.scala
@@ -1,5 +1,42 @@
 package test.scala.predict
 
-class CovarianceValuePredictorWithDITest {
+import main.scala.predict.CovarianceValuePredictor
+import test.scala.application.DITestBase
+import main.scala.predict.HDayMCSValuePredictor
+
+class CovarianceValuePredictorWithDITest extends DITestBase {
+
+  val instanceBeanName = "covarianceValuePredictor"
+
+  var instance: CovarianceValuePredictor = _
+
+  override def beforeAll(): Unit = {
+
+    super.beforeAll
+  }
+
+  override def beforeEach() {
+
+    generateInstance
+  }
+
+  // Overridden to prevent Spark Context from being recycled
+  override def afterEach() {}
+
+  //
+  //  Start of tests
+  //  **************
+  test("test construction of instance from DI") {
+    println(s"${instance}")
+  }
+  
+    //
+  //  Helper functions
+  //  ****************
+  private def generateInstance = {
+
+    super.generateApplicationContext
+    instance = ctx.getBean(instanceBeanName).asInstanceOf[CovarianceValuePredictor]
+  }
   
 }
